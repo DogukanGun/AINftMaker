ea0c526f4350f35968bd5f175990fd72288e4460
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-07-04 11:02:58 +0300

    Upload page design

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,55 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 
+declare global {
+    interface Window {
+        phantom?: any;
+    }
+}
+
 const CustomNavbar = () => {
+    const [isPhantomInstalled, setIsPhantomInstalled] = useState(false);
+    const [isPhantomConnected, setIsPhantomConnected] = useState(false);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        setIsPhantomInstalled(!!(window && window.phantom && window.phantom.solana && window.phantom.solana.isPhantom));
+    }, []);
+    const getProvider = () => {
+        if ('phantom' in window) {
+            const provider = window.phantom?.solana;
+
+            if (provider?.isPhantom) {
+                return provider;
+            }
+        }
+        window.open('https://phantom.app/', '_blank');
+    };
+    const connectWallet = async () => {
+        const provider = getProvider(); // see "Detecting the Provider"
+        try {
+            const resp = await provider.connect();
+            console.log(resp.publicKey.toString());
+            setIsPhantomConnected(true);
+            // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo 
+        } catch (err) {
+            // { code: 4001, message: 'User rejected the request.' }
+        }
+    }
+
+    const navigateCreateRouter = () =>{
+        router.push("/nft/create");
+    }
+
+    const buttonClicked = () => {
+        if(isPhantomConnected){
+            connectWallet();
+        }else{
+            navigateCreateRouter();
+        }
+    }
     return (
         <nav className=" border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -10,10 +58,17 @@ const CustomNavbar = () => {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Flowbite</span>
                 </a>
                 <div className="flex md:order-2">
-                    <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Connect Wallet</button>
+                    {isPhantomInstalled &&
+                        <button type="button" onClick={buttonClicked} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                            {isPhantomConnected ? "Connect Wallet" : "Create NFT"}
+                        </button>
+                    }
+                    {!isPhantomInstalled &&
+                        <a href="https://phantom.app/" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Please Install Phantom Wallet</a>
+                    }
                     <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg>
                     </button>
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
